a45439f29cde3aa3483690749170a202dd08606c
arp123-000 <arp.pur.rt22@dypatil.edu>
2025-04-30 06:32:46 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,8 @@
+ public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10; // first number
+        int num2 = 20; // second number
+        int sum = num1 + num2; // addition
+        System.out.println("The sum is: " + sum);
+    }
+}
